cba03842627aa03dc6133b7d758ab649403b260c
Estelle DeBlois <edeblois@gmail.com>
2015-03-12 20:06:20 -0400

    Allow libraries like moment.js to attach to window instead of global.
    Closes #5

--- blueprints/node-webkit/files/vendor/node-webkit/shim.js
+++ blueprints/node-webkit/files/vendor/node-webkit/shim.js
@@ -1,11 +1,19 @@
 (function(window) {
   if (!window.nwDispatcher) return;
 
+  // Move `global.window` out of the way as it causes some third-party
+  // libraries to expose themselves via `global` instead of `window`.
+  var globalWindow = global.window;
+  delete global.window;
+
   // Rename node's `require` to avoid conflicts with AMD's `require`.
   var requireNode = window.requireNode = window.require;
   delete window.require;
 
   window.addEventListener('load', function() {
+    // Restore `global`.
+    global.window = globalWindow;
+
     // Once the document is loaded, make node's `require` function
     // available again. This is required for certain node modules that
     // can't work with the alias, and for nw.js's GUI to function properly
